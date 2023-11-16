b69170f5fd1bc78cc6f708dc3098caf9c5831f89
alladio_bonesso@hotmail.com <alladio_bonesso@hotmail.com>
2023-11-16 20:17:45 +0000

    add merge between tables

--- customers01.py
+++ customers01.py
@@ -24,3 +24,21 @@
 
 # MAGIC %sql
 # MAGIC DROP TABLE customers_silver
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC CREATE TABLE IF NOT EXISTS customers_silver_merged
+# MAGIC (customer_id int,
+# MAGIC tax_id double,
+# MAGIC tax_code string,
+# MAGIC customer_name string,
+# MAGIC state string,
+# MAGIC city string,
+# MAGIC postcode string,
+# MAGIC street string);
+# MAGIC MERGE INTO customers_silver_merged
+# MAGIC   USING customer_bronze
+# MAGIC   ON customers_bronze.customer_id = customers_silver_merged.customer_id
+# MAGIC   WHEN NOT MATCHED THEN INSERT *;
+# MAGIC
