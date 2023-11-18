b60e44f98d42282e15bdf85c3a4ded68c2c587d0
Alladio Bonesso <Alladio@users.noreply.github.com>
2023-11-18 19:32:52 -0300

    create file handle  Json

---
+++ 04 - manipulando JSON.py
@@ -0,0 +1,32 @@
+# Databricks notebook source
+# MAGIC %fs ls /databricks-datasets/structured-streaming/events/
+
+# COMMAND ----------
+
+# MAGIC %fs head /databricks-datasets/structured-streaming/events/file-1.json
+
+# COMMAND ----------
+
+# MAGIC %python
+# MAGIC #lendo 1 arquivo JSON
+# MAGIC df = spark.read.json("/databricks-datasets/structured-streaming/events/file-1.json")
+# MAGIC df.printSchema()
+# MAGIC df.show()
+
+# COMMAND ----------
+
+# MAGIC %python
+# MAGIC #lendo dois arquivos JSON
+# MAGIC df2.spark.read.json(["/databricks-datasets/structured-streaming/events/file-1.json","/databricks-datasets/structured-streaming/events/file-2.json"])
+# MAGIC df2.show()
+
+# COMMAND ----------
+
+# MAGIC %python
+# MAGIC #lendo dois arquivos JSON
+# MAGIC df3.spark.read.json("/databricks-datasets/structured-streaming/events/*.json")
+# MAGIC df3.show()
+
+# COMMAND ----------
+
+
