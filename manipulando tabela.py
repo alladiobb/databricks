eebb1baaac5074d86c4a0009d61c5f9f9ae80395
Alladio Bonesso <Alladio@users.noreply.github.com>
2023-11-18 10:23:19 -0300

    Load table

--- manipulando tabela.py
+++ manipulando tabela.py
@@ -17,3 +17,20 @@ add("endereco",StringType()).
 add("variante",StringType()).
 add("vinicola",StringType())
 )
+
+#Load table
+# File location and type
+file_location = "/FileStore/tables/carga/vinhos_no_mundo.csv"
+file_type = "csv"
+# CSV options
+infer_schema = "false"
+first_row_is_header = "true"
+delimiter = ","
+# The applied options are for CSV files. For other file types,
+these will be ignored.
+df = spark.read.format(file_type).schema(Record_schema) \
+.option("inferSchema", infer_schema) \
+.option("header", first_row_is_header) \
+.option("sep", delimiter) \
+.load(file_location)
+display(df)
